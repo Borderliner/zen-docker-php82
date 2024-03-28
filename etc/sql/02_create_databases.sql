880f27a0714fdd943bf145833de50f2aed0f1390
Reza Hajianpour <hajianpour.mr@gmail.com>
2024-03-28 15:54:30 +0330

    Update 02_create_databases.sql

--- etc/sql/02_create_databases.sql
+++ etc/sql/02_create_databases.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS smm_dev COLLATE 'utf8mb4_unicode_ci';;
-CREATE DATABASE IF NOT EXISTS smm_prod COLLATE 'utf8mb4_unicode_ci';;
-CREATE DATABASE IF NOT EXISTS smm_test COLLATE 'utf8mb4_unicode_ci';;
+CREATE DATABASE IF NOT EXISTS app_dev COLLATE 'utf8mb4_unicode_ci';;
+CREATE DATABASE IF NOT EXISTS app_prod COLLATE 'utf8mb4_unicode_ci';;
+CREATE DATABASE IF NOT EXISTS app_test COLLATE 'utf8mb4_unicode_ci';;
