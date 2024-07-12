0082f1a4678aca31723900bc4bbc8dfaf92b69f0
Mohammadreza Hajianpour <hajianpour.mr@gmail.com>
2024-07-13 03:28:44 +0330

    Update gitignore

---
+++ etc/mysql/init/02_create_databases.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS app_dev COLLATE 'utf8mb4_unicode_ci';;
+CREATE DATABASE IF NOT EXISTS app_prod COLLATE 'utf8mb4_unicode_ci';;
+CREATE DATABASE IF NOT EXISTS app_test COLLATE 'utf8mb4_unicode_ci';;
